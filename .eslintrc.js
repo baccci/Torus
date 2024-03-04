ac5d795614cce0cf9076d78044460d863b665a25
Salvador Bacci <salvador_bacci@hotmail.com>
2024-03-04 07:05:36 -0300

    improved linting

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [ 'eslint-config-astro-svelte-jsdoc-standard' ],
-  parserOptions: { tsconfigRootDir: __dirname },
-  rules: {
-    "no-multiple-empty-lines": ["error", { "max": 1, "maxBOF": 0, "maxEOF": 0 }]
-  }
-}
