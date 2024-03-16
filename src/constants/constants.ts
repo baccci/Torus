01a2a3987f611e6299df459818b8691703631ce7
Salvador Bacci <salvador_bacci@hotmail.com>
2024-03-16 05:00:47 -0300

    added new constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,7 @@
-export const HALF_PI = Number((Math.PI / 2).toFixed(3))
 export const PI = Number(Math.PI.toFixed(3))
+export const HALF_PI = Number((Math.PI / 2).toFixed(3))
+export const QUARTER_PI = Number((Math.PI / 4).toFixed(3))
+export const THREE_QUARTERS_PI = Number((3 * Math.PI / 4).toFixed(3))
 export const CHANGE_RATE = 0.05
 export const POINT_SHAPES = {
   CIRCLE: 'circle',
